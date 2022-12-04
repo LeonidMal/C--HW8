2a809f22df0107c652a32c15bbedff790e788587
Leonid <rockwil91@gmail.com>
2022-12-04 21:01:16 +0200

    Д/З 8 - задача 56

---
+++ task 0/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arry)
+{
+    for (int i = 0; i < arry.GetLength(0); i++)
+    {
+        for (int j = 0; j < arry.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int GetInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int row = GetInt("Введите количество строк: ");
+int column = GetInt("Введите количество столбцов: ");
+
+int[,] array = new int[row, column];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+SortArray(array);
+PrintArray(array);
+
+void SortArray(int[,] ar)
+{
+    int tempNum = 0;
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1) - 1; j++)
+        {
+            if (ar[i, j] < ar[i, j + 1])
+            {
+                tempNum = ar[i, j];
+                ar[i, j] = ar[i, j + 1];
+                ar[i, j + 1] = tempNum;
+            }
+        }
+    }
+}
---
+++ task 1/Program.cs
@@ -0,0 +1,95 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+int GetInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Fill2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void Print2DArray(int[,] arry)
+{
+    for (int i = 0; i < arry.GetLength(0); i++)
+    {
+        for (int j = 0; j < arry.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int row = GetInt("Введите количество строк: ");
+int column = GetInt("Введите количество столбцов: ");
+int[,] array = new int[row, column];
+int sum = 0;
+int[] sumArray = new int[row];
+int index = sumArray[0];
+
+Fill2DArray(array);
+Print2DArray(array);
+Console.WriteLine();
+Sum(array);
+ArrayComp(sumArray);
+Console.Write($"{index + 1} строка");
+
+void Sum(int[,] arry)
+{
+    for (int i = 0; i < arry.GetLength(0); i++)
+    {
+        for (int j = 0; j < arry.GetLength(1); j++)
+        {
+            sum = sum + arry[i, j];
+        }
+        sumArray[i] = (sum);
+        sum = 0;
+    }
+}
+
+void ArrayComp(int[] r)
+{
+    for (int i = 0; i < 5 - 1; i++)
+    {
+        if (r[i] > r[i + 1])
+            index = i + 1;
+    }
+}
+
+
+/*
+Console.WriteLine();
+PrintArray(sumArray);                           //проверка сумм
+void PrintArray(int[] r)
+{
+    for (int i = 0; i < r.Length; i++)
+    {
+        Console.Write($"{r[i]} ");
+    }
+}
+*/
+
+
