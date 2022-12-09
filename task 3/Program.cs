4dab1f7a383bd203fb20f1cd22b355462a71f1ca
Leonid <rockwil91@gmail.com>
2022-12-09 23:31:23 +0200

    Д/З 8 - Задача 58

---
+++ task 3/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+
+int GetInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int row = GetInt("Введите количестов строк: ");
+int column = GetInt("Введите количестов столбцов: ");
+int[,] firstMatrix = new int[row, column];
+int[,] secondMatrix = new int[row, column];
+int[,] prodMatrix = new int[row, column];
+
+
+FillMatrix(firstMatrix);
+FillMatrix(secondMatrix);
+
+PrintMatrix(firstMatrix);
+Console.WriteLine();
+PrintMatrix(secondMatrix);
+
+ProdMatrix(firstMatrix, secondMatrix);
+Console.WriteLine();
+PrintMatrix(prodMatrix);
+
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ProdMatrix(int[,] fMatrix, int[,] sMatrix)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            for (int k = 0; k < column; k++)
+            {
+                prodMatrix[i,j] += fMatrix[i,k] * sMatrix[k, j];
+            }
+        }
+    }
+}
